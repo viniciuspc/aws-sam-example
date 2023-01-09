dca5875d7d97217f34c19afb2a5ddf457d861ad1
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2023-01-09 16:07:52 +0000

    Include DynamoDB Table

--- src/app.py
+++ src/app.py
@@ -4,6 +4,11 @@ import os
 
 print('Loading function')
 
+#create the client outside of the handler
+region_name = os.environ['REGION_NAME']
+dynamo = boto3.client('dynamodb', region_name=region_name)
+table_name = os.environ['TABLE_NAME']
+
 
 def respond(err, res=None):
     return {
@@ -17,4 +22,5 @@ def respond(err, res=None):
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
-    return respond(None, res="hello world!")
+    scan_result = dynamo.scan(TableName=table_name)
+    return respond(None, res=scan_result)
